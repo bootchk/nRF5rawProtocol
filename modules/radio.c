c50dcd49f06985d2a8bc27877b0056240a627f87
bootchk <bootch@nc.rr.com>
2016-09-02 18:07:47 -0400

    work on Radio class

--- modules/radio.c
+++ modules/radio.c
@@ -5,12 +5,70 @@
 
 void Radio::configure() {
 
+	// FUTURE: parameter
 	// Freq is one of 3 BT advertising channels
 	NRF_RADIO->FREQUENCY = 2;	// or 26, 80
 
 	// Whitening depends on freq?
-	NRF_RADIO->DATAWHITEIV = 37 & RADIO_DATAWHITEIV_DATAWHITEIV_Msk;
+	NRF_RADIO->DATAWHITEIV = 37 & RADIO_DATAWHITEIV_DATAWHITEIV_Msk;	// or 38, 39
+
+	// FUTURE: parameter
+	// TODO address
+	// Receive same address we transmit. (But we don't receive our own transmissions.)
+	// Receive only logical address 0 (or 1?) i.e. address configured by
+	NRF_RADIO->RXADDRESSES = 0x01;
+
+	//NRF_RADIO->PREFIX1	= ((m_alt_aa >> 24) & 0x000000FF);
+	//NRF_RADIO->BASE1    = ((m_alt_aa <<  8) & 0xFFFFFF00);
+
+	// FUTURE: parameter
+	// Default tx power
 };
+
+void Radio::transmit(void * data){
+	setupXmitOrRcv(data);
+	startXmit();
+};
+
+void Radio::turnOnReceiver() {
+	int data;	// TODO allocate buffers
+
+	setupXmitOrRcv(&data);
+	startRcv();
+}
+
+
+void Radio::setupXmitOrRcv(void * data) {
+	// Setup common to xmit or rcv
+
+	// setup shortcuts, i.e. avoid some events
+	NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_DISABLE_Msk | RADIO_SHORTS_ADDRESS_RSSISTART_Msk;
+
+	NRF_RADIO->PACKETPTR = (uint32_t) data;
+	// interrupt enable ???
+	NRF_RADIO->INTENSET = RADIO_INTENSET_END_Msk;
+
+	NRF_RADIO->EVENTS_END = 0;
+	//NRF_RADIO->PREFIX1	= ((m_alt_aa >> 24) & 0x000000FF);
+	//NRF_RADIO->BASE1    = ((m_alt_aa <<  8) & 0xFFFFFF00);
+}
+
+/*
+ * In task/event architecture, these trigger or enable radio's tasks.
+ *
+ * Not keeping our own state (radio peripheral device has a state.)
+ */
+void Radio::startXmit() {
+	// require radio state is
+	NRF_RADIO->TASKS_TXEN = 1;
+	// assert radio state is
+}
+
+void Radio::startRcv() {
+    NRF_RADIO->TASKS_RXEN = 1;
+	// assert radio state is
+}
+
 
 /*
  * static void setup_event(radio_event_t* p_evt)
--- modules/radio.h
+++ modules/radio.h
@@ -1,6 +1,18 @@
 
+/*
+ * Driver for radio peripheral (understands registers, etc.)
+ *
+ * Singleton, all static class methods.
+ */
 class Radio {
 
 public:
 	static void configure();
+	static void transmit(void * data);
+	static void turnOnReceiver();
+	// TODO turnOFF
+private:
+	static void setupXmitOrRcv(void * data);
+	static void startXmit();
+	static void startRcv();
 };
